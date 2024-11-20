ac01757d9989ab1f5751468bc08c8f8de6c1af70
fatihbaydar <fatihbaydar2004@gmail.com>
2024-11-20 10:45:33 +0300

    create first server

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 "use strict"
-console.log("nodejs server")
+// console.log("nodejs server")
 
+const http = require("node:http")
+
+// http.createServer((request,response) => {
+
+// })
+//! server oluştur
+// http.createServer((req,res) => {
+//     console.log("serverdasın")
+//     res.end("hello nodejs")
+// })
+
+//! gelen istekleri dinlemeye başla
+
+/*
+const app = http.createServer((req,res) => {
+    console.log("serverdasın")
+    res.end("<h1>hello nodejs</h1>")
+})
+
+app.listen(8000,"127.0.0.1") */
+
+const app = http.createServer((req,res) => {
+    console.log("serverdasın")
+    res.end("<h1>hello nodejs</h1>")
+}).listen(8000,"127.0.0.1")
