545519fd42f3a8a4bddc90f3074c0ba504a39d40
fatihbaydar <fatihbaydar2004@gmail.com>
2024-11-20 13:54:27 +0300

    dotenv,gitignore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-"use strict"
+"use strict";
 // console.log("nodejs server")
 
-const http = require("node:http")
+const http = require("node:http");
 
 // http.createServer((request,response) => {
 
@@ -22,7 +22,19 @@ const app = http.createServer((req,res) => {
 
 app.listen(8000,"127.0.0.1") */
 
+/*
 const app = http.createServer((req,res) => {
-    console.log("serverdasın")
+    // console.log("serverdasın")
     res.end("<h1>hello nodejs</h1>")
-}).listen(8000,"127.0.0.1")
+}).listen(8000,"127.0.0.1", () => console.log("server runned http://127.0.0.1:8000"))
+*/
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000
+const HOST = process.env.HOST || "127.0.0.1"
+
+const app = http
+  .createServer((req, res) => {
+    res.end("<h1>hello nodejs</h1>");
+  })
+  .listen(PORT, () => console.log(`server runned http://${HOST}:${PORT}`));
